0f0aa7d9a0ab74600f893fe0eafa986c754c5cdb
Kumagai Miki <kumagai@tierline.com>
2026-08-19 21:54:02 +0900

    修正

--- dbt/learning_data_platform/models/marts/weekly_average.sql
+++ dbt/learning_data_platform/models/marts/weekly_average.sql
@@ -2,8 +2,8 @@
 
 SELECT
     progress_date,
-    daily_progress,
-    AVG(daily_progress) OVER (
+    daily_learning,
+    AVG(daily_learning) OVER (
         ORDER BY progress_date
         ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
     ) AS seven_day_average
